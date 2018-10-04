8734f8d668174299733916b34943bc201f4cdaa4
Naved <naved1234@gmail.com>
2018-10-04 18:16:37 +0530

    code addition

---
+++ Data Structures/Linked List/LinkedList.java
@@ -0,0 +1,41 @@
+public class LinkedList {
+  private static class Node {
+    int data;
+    Node next;
+
+    public Node(int data) { this.data = data; }
+  }
+
+  private Node head;
+
+  public void addFront(int data) {
+    Node newNode = new Node(data);
+    if (head == null) {
+      head = newNode;
+      return;
+    }
+    newNode.next = head;
+    head = newNode;
+  }
+
+  public int getFirst() {
+    if (head == null) {
+      throw new IllegalStateException("Empty list!");
+    }
+    return head.data;
+  }
+  
+  public int getLast() {
+    if (head == null) {
+      throw new IllegalStateException("Empty list!");
+    }
+    Node current = head;
+    
+    while (current.next != null) {
+      current = current.next;
+    }
+
+    return current.data;
+  }
+
+}
